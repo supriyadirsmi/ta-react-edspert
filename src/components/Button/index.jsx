adb2d8fbc82873c7d07ac956e1078be0b4aae4fc
suprigospurr <supri@gospurr.com>
2023-03-29 06:17:54 +0700

    Rename css

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './styles.module.css'
+import styles from './button.module.css'
 
 export default function Button(props) {
   return ( <button className={props.variant === 'signup' && styles['btn-signup']}>{props.text}</button> )
