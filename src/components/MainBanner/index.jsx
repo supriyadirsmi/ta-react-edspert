2d7cd7748769775959636f22c07299dc00fd78ed
suprigospurr <supri@gospurr.com>
2023-03-29 01:01:11 +0700

    Update styling

--- src/components/MainBanner/index.jsx
+++ src/components/MainBanner/index.jsx
@@ -6,7 +6,7 @@ export default function MainBanner() {
         <div className='container-banner flex'>
             <div className='basis-1/2'>
                 <h1>Jadi expert bersama edspert.id</h1>
-                <p className='desc-text mt-[27px]'>Tingkatkan skill dan pengetahuan bersama para mentor terbaik di bidangnya, untuk siapkan karir impian anda.</p>
+                <p className='desc-text mt-[27px] w-[90%]'>Tingkatkan skill dan pengetahuan bersama para mentor terbaik di bidangnya, untuk siapkan karir impian anda.</p>
             </div>
             <div className='basis-1/2 flex justify-center'>
                 <img src={modelBanner} alt='Tugas Akhir Edspert React Batch 3 Supriyadi'/>
